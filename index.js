ce5b047e715a2d7d96fe47d5ba79d599f0a4e353
zain.noorani <zain.noorani@laksol.com>
2025-10-27 18:06:56 +0500

    changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("./models");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -18,4 +19,4 @@ sequelize.sync()
   .then(() => console.log("✅ Tables created"))
   .catch((err) => console.error("❌ Error creating tables:", err));
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
